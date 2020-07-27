c6e737c0e1bddc5a965d7a5535ba4a2384e0c3ee
Patrik Nusszer <patrik.nusszer@icloud.com>
2020-07-27 08:59:39 +0200

    Initial Commit

---
+++ lilcryptocpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lilcryptocpp
+//
+//  Created by Patrik Nusszer on 2020. 07. 27..
+//  Copyright © 2020. Patrik Nusszer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
